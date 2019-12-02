50110134a9d7609859ca6313fc05c7fae0b246ea
l <l@l.local>
2019-12-02 20:23:48 +0800

    init repo

---
+++ src/test/java/com/eckingl/communtiy/CommuntiyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eckingl.communtiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuntiyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
